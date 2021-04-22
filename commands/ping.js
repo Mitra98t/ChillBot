88c001bb56501f4e3a2187d86f1fe38b8e74aba7
Mitra98t <personalmailfm98@gmail.com>
2021-04-23 01:33:41 +0200

    bot init

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'ping',
+    description: 'This is a FUCKING PING PONG COMMAND BABY',
+    execute(msg, args, discord){
+        let embed = new discord.MessageEmbed()
+            .setColor('#'+Math.floor(Math.random()*16777215).toString(16))
+            .setTitle(':ping_pong:  Pong!')
+            .addField('Da invio comando a\nricezione risposta', `${Date.now() - msg.createdTimestamp}ms`, true);
+
+        msg.channel.send(embed);
+    }
+}
---
+++ main.js
@@ -0,0 +1,36 @@
+const TOKEN = require('./privateInfo.js')
+const discord = require('discord.js');
+const client = new discord.Client();
+const pfx = '_';
+
+const fs = require('fs');
+
+client.commands = new discord.Collection();
+const commandFile = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+//INIT
+for (file of commandFile) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+//starting
+client.once('ready', () => {
+    console.log('ChillBot è online');
+})
+
+client.on('message', message => {
+    if (!message.content.startsWith(pfx) || message.author.bot) return;
+
+    const args = message.content.slice(pfx.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case ('ping'):
+            client.commands.get('ping').execute(message, args, discord);
+            break;
+    }
+})
+
+client.login(TOKEN);
