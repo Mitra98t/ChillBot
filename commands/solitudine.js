3e029ac3220173116f9064d5b94e10fcde049405
Mitra98t <personalmailfm98@gmail.com>
2021-04-28 00:58:39 +0200

    Pex controll for admins on _solitudine #4

--- commands/solitudine.js
+++ commands/solitudine.js
@@ -1,9 +1,14 @@
+const { normalize } = require('ffmpeg-static')
 const Util = require('../utility')
 
 module.exports = {
     name: 'solitudine',
     description: 'Ora siete da soli',
     execute(msg, args, discord){
+        if(!(msg.member.roles.cache.some(r => r.name === "Capo Crew") || msg.member.roles.cache.some(r => r.name === "Admin"))){
+            return msg.channel.send(Util.Reply.errorEmbed('Mi sa di no...', 'Non hai permessi sufficienti per usare questo comando'))
+        }
+
         const chatToAbuse = msg.member.voice.channel
         if (!chatToAbuse) return msg.channel.send(Util.Reply.sendBaseEmbed('Mmm...', 'Ti è andata bene...'))
 
