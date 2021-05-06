f0753ba4dbc7d7949ad549b9d426b60fa852affb
Mitra98t <personalmailfm98@gmail.com>
2021-05-07 01:39:49 +0200

    cats and cats text escaped chars

--- commands/cat.js
+++ commands/cat.js
@@ -2,11 +2,19 @@ const Util = require('../utility')
 
 module.exports = {
     name: 'cat',
-    description: 'Get a random cat photo', // It isn't really, I just use this to try stuff.
+    description: 'Get a random cat photo', 
     execute(msg, args, discord) {
+        let baseURL = 'https://cataas.com/cat'
+        let saysURL = '/says/'
+        let text = ""
+        if(args.length){
+            text = args.map(x => escape(x))
+            text = text.join('%20')
+        }
+
         if(args.length)
-            msg.channel.send(Util.Reply.sendBaseImage(`Cat says: ${args.join(' ')}`,null,`https://cataas.com/cat/says/${args.join('⠀')}`))
+            msg.channel.send(Util.Reply.sendBaseImage(`Cat says: ${args.join(' ')}`,null,`${baseURL}${saysURL}${text}`))
         else
-            msg.channel.send(Util.Reply.sendBaseImage('Cat',null,`https://cataas.com/cat`))
+            msg.channel.send(Util.Reply.sendBaseImage('Cat',null,baseURL))
     }
 }
