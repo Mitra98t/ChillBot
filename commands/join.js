de6ef56a01777db75d774e4c58c257eb432663c7
Mitra98t <personalmailfm98@gmail.com>
2021-05-10 16:58:54 +0200

    auto exit on error

--- commands/join.js
+++ commands/join.js
@@ -24,6 +24,7 @@ module.exports = {
             const dispatcher = connection.play(song);
             dispatcher.on("finish", finish => msg.guild.me.voice.channel.leave());
         }).catch(e => {
+            msg.guild.me.voice.channel.leave()
             console.error(e);
         });
 
